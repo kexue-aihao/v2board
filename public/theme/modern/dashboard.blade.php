f3ecdd2e22953cd76cbcf3cef3ef4679e0fb250b
kexue-aihao <weacrcr@proton.me>
2026-07-23 22:06:42 +0800

    feat: 新增 Vue 现代化用户主题

---
+++ public/theme/modern/dashboard.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,viewport-fit=cover">
+    <meta name="theme-color" content="#f4f7f5">
+    <link rel="stylesheet" href="/theme/{{$theme}}/assets/index.css?v={{$version}}">
+    <title>{{$title}}</title>
+    <script>window.routerBase = "/";</script>
+    <script>
+        window.settings = {
+            title: @json($title),
+            assets_path: '/theme/{{$theme}}/assets',
+            theme: {
+                sidebar: @json($theme_config['theme_sidebar'] ?? 'light'),
+                header: @json($theme_config['theme_header'] ?? 'light'),
+                color: @json($theme_config['theme_color'] ?? 'default')
+            },
+            version: @json($version),
+            background_url: @json($theme_config['background_url'] ?? ''),
+            description: @json($description),
+            i18n: ['zh-CN', 'en-US', 'ja-JP', 'vi-VN', 'ko-KR', 'zh-TW', 'fa-IR'],
+            logo: @json($logo)
+        };
+    </script>
+</head>
+<body>
+<div id="app"></div>
+{!! $theme_config['custom_html'] ?? '' !!}
+<script type="module" src="/theme/{{$theme}}/assets/index.js?v={{$version}}"></script>
+</body>
+</html>
